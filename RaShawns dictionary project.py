f820ceb36e3c5a0c5577af74fbc072b8ab692bfa
rashawn-092503 <42866937+rashawn-092503@users.noreply.github.com>
2018-09-17 08:31:56 -0400

    Add files via upload

---
+++ RaShawns dictionary project.py
@@ -0,0 +1,19 @@
+current_health = 200
+input("press (f) to get loadout informatioin")
+if 
+loadout = {
+    'primary_weapon' : "Assult Rifles" # make sure th program wworks bfore making more complex
+    'seconday_weapon' : "Pistols" # make sure th program wworks bfore making more complex
+    'perks' : "Quick revive, Scavenger"
+    'medkit' : "1"
+    'current_exp' : 5000
+    'exp_need_to_level' : 20000
+def lose_health ():
+    
+
+
+while True : current_health > 0:
+    
+    
+        
+    
